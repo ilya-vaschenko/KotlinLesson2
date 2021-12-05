be060d5bc93f3c93007207b72265904532ee3e3a
Илья Александрович <vaschenkoilya@yandex.ru>
2021-12-06 00:37:49 +0700

    домашнее задание 9

--- app/src/main/java/com/example/kotlinlesson2/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson2/view/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.kotlinlesson2.view
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.kotlinlesson2.R
 import com.example.kotlinlesson2.databinding.MainActivityBinding
 
@@ -11,6 +19,54 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: MainActivityBinding by lazy {
         MainActivityBinding.inflate(layoutInflater)
+    }
+
+    private val permissionResult =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            when {
+                result -> getLocation()
+
+                !ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) -> {
+
+                    AlertDialog.Builder(this)
+                        .setTitle("Доступ к геолокации")
+
+                        .setMessage(
+                            "Вы запретили доступ к геолокации! \n" +
+                            "Ваше местоположение не ннайдено!\n" +
+                            "Открыть доступ?"
+                        )
+
+                        .setPositiveButton("Открыть настройки") { _, _ ->
+                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+
+                            val uri: Uri = Uri.fromParts("package", packageName, null)
+                            intent.data = uri
+                            startActivity(intent)
+                        }
+                        .setNegativeButton("Нет") { dialog, _ -> dialog.dismiss() }
+                        .create()
+                        .show()
+
+                }
+                else -> Toast.makeText(this, "No access to permission", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun getLocation() {
+
+        AlertDialog.Builder(this)
+            .setTitle("Геолокация открыта")
+
+            .setMessage("Ваше местонахождение найдено \n")
+
+            .setNegativeButton("ok!") { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +86,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
+        return when (item.itemId) {
             R.id.idHistory -> {
                 supportFragmentManager.apply {
                     beginTransaction()
@@ -38,6 +94,10 @@ class MainActivity : AppCompatActivity() {
                         .addToBackStack("")
                         .commitAllowingStateLoss()
                 }
+                true
+            }
+            R.id.getGeoPosition -> {
+                permissionResult.launch(Manifest.permission.ACCESS_FINE_LOCATION)
                 true
             }
             else -> super.onOptionsItemSelected(item)
